14c9f045422e5fe34b6403fa14baaa0f859e4123
Anthony H <anthony.hadwan@hec.edu>
2025-02-16 22:23:41 +0100

    worked on meals component and added dummy data to db

--- 01-Essentials/Project/components/main-header/nav-link.js
+++ 01-Essentials/Project/components/main-header/nav-link.js
@@ -1,5 +1,7 @@
 "use client";
 
+// This was in the header but we moved it as client side rendering should be pushed the farthest possible on the component tree
+
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
