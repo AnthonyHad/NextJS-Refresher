3291fee8c36e7b34a1fd0ca3c1717af27d9d36e9
Anthony H <anthony.hadwan@hec.edu>
2025-04-12 22:17:01 +0200

    added server side validation for form submission and some loading feedback

--- 01-Essentials/Project/lib/actions.js
+++ 01-Essentials/Project/lib/actions.js
@@ -3,6 +3,10 @@
 import { redirect } from "next/navigation";
 import { saveMeal } from "./meals";
 
+function isInvalidText(text) {
+  return !text || text.trim() === "";
+}
+
 export async function shareMeal(formData) {
   // Creates a server action only executed on a server
   //   "use server";
@@ -15,6 +19,19 @@ export async function shareMeal(formData) {
     creator_email: formData.get("email"),
     image: formData.get("image"),
   };
+  // Server side validation. Can use some validation library if needed
+  if (
+    isInvalidText(meal.title) ||
+    isInvalidText(meal.summary) ||
+    isInvalidText(meal.instructions) ||
+    isInvalidText(meal.creator) ||
+    isInvalidText(meal.creator_email) ||
+    !meal.creator_email.includes("@") ||
+    !meal.image ||
+    meal.image.size === 0
+  ) {
+    throw new Error("Invalid input");
+  }
 
   await saveMeal(meal);
 
