77ebfb01d53c126404f91b3c18074b63ed78a0e1
Anthony H <anthony.hadwan@hec.edu>
2025-04-20 14:03:22 +0200

    added route groups

--- 02-Routing-Rendering-DeepDive/app/(content)/news/[id]/@modal/(.)image/page.js
+++ 02-Routing-Rendering-DeepDive/app/(content)/news/[id]/@modal/(.)image/page.js
@@ -1,7 +1,12 @@
+"use client";
+
 import { DUMMY_NEWS } from "@/dummy-news";
 import { notFound } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 export default function InterceptedImagePage({ params }) {
+  const router = useRouter();
+
   const newsId = params.id;
   const newsItem = DUMMY_NEWS.find((news) => news.id === newsId);
 
@@ -11,7 +16,7 @@ export default function InterceptedImagePage({ params }) {
 
   return (
     <>
-      <div className="modal-backdrop" />
+      <div className="modal-backdrop" onClick={router.back} />
       <dialog className="modal" open>
         <div className="fullscreen-image">
           <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
