1db1095493a4f4bf84752e5be06247e430ceac35
Anthony H <anthony.hadwan@hec.edu>
2025-04-29 10:52:36 +0200

    added basic authentication

--- 05-Authentication/actions/auth-actions.js
+++ 05-Authentication/actions/auth-actions.js
@@ -1,1 +1,38 @@
 "use server";
+
+import { hashUserPassword } from "@/lib/hash";
+import { createUser } from "@/lib/user";
+import { redirect } from "next/dist/server/api-utils";
+
+export async function signup(prevState, formData) {
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  let errors = {};
+
+  if (!email.includes("@")) {
+    errors.email = "Please enter a valid email address.";
+  }
+  if (password.trim().length < 8) {
+    errors.password = "Password must be at least 8 characters long.";
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return { errors };
+  }
+
+  const hashedPassword = hashUserPassword(password);
+
+  try {
+    createUser(email, hashedPassword);
+  } catch (error) {
+    if (error.code === "SQLITE_CONSTRAINT_UNIQUE") {
+      errors.email = "Email already exists.";
+      return { errors };
+    } else {
+      throw error;
+    }
+  }
+
+  redirect("/training");
+}
